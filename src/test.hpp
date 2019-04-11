4c41e4cc72e2476cd151f18479168aa6167e85a3
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2019-04-12 00:10:38 +0200

    implement further markers

--- src/test.hpp
+++ src/test.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include <iostream>
 #include <vector>
 
@@ -27,6 +28,12 @@ struct Test_output {
    double false_discovery_rate{0.};
    double false_omission_rate{0.};
 
+   double accuracy{0.};
+   double f1_score{0.};
+   double matthews_correlation_coefficient{0.};
+   double bookmaker_informednes{0.};
+   double markednes{0.};
+
    double mean_diff{0.0};
 
    friend std::ostream& operator<<(std::ostream&, const Test_output&);
@@ -37,20 +44,25 @@ std::ostream& operator<<(std::ostream& ostr, const Test_output& t)
    ostr << "Test results\n"
            "============\n"
         << "sample size = " << t.sample_size << '\n'
-        << "positive = " << t.positive << '\n'
-        << "negative = " << t.negative << '\n'
-        << "true positive = " << t.true_positive << '\n'
-        << "true negative = " << t.true_negative << '\n'
-        << "false positive = " << t.false_positive << '\n'
-        << "false negative = " << t.false_negative << '\n'
-        << "true positive rate = " << t.true_positive_rate << '\n'
-        << "true negative rate = " << t.true_negative_rate << '\n'
-        << "false positive rate = " << t.false_positive_rate << '\n'
-        << "false negative rate = " << t.false_negative_rate << '\n'
-        << "positive predicive value = " << t.positive_predicive_value << '\n'
-        << "negative predicive value = " << t.negative_predicive_value << '\n'
-        << "false discovery rate = " << t.false_discovery_rate << '\n'
-        << "false omission rate = " << t.false_omission_rate << '\n'
+        << "positive (P) = " << t.positive << '\n'
+        << "negative (N) = " << t.negative << '\n'
+        << "true positive (TP) = " << t.true_positive << '\n'
+        << "true negative (TN) = " << t.true_negative << '\n'
+        << "false positive (FP) = " << t.false_positive << '\n'
+        << "false negative (FN) = " << t.false_negative << '\n'
+        << "true positive rate (TPR) = " << t.true_positive_rate << '\n'
+        << "true negative rate (TNR) = " << t.true_negative_rate << '\n'
+        << "false positive rate (FPR) = " << t.false_positive_rate << '\n'
+        << "false negative rate (FNR) = " << t.false_negative_rate << '\n'
+        << "positive predicive value (PPV) = " << t.positive_predicive_value << '\n'
+        << "negative predicive value (NPV) = " << t.negative_predicive_value << '\n'
+        << "false discovery rate (FDR) = " << t.false_discovery_rate << '\n'
+        << "false omission rate (FOR) = " << t.false_omission_rate << '\n'
+        << "accuracy (ACC) = " << t.accuracy << '\n'
+        << "F1 score = " << t.f1_score << '\n'
+        << "matthews correlation coefficient (MCC) = " << t.matthews_correlation_coefficient << '\n'
+        << "bookmaker informednes (BM) = " << t.bookmaker_informednes << '\n'
+        << "markednes (MK) = " << t.markednes << '\n'
         << "mean abs diff = " << t.mean_diff << " (should be < 0.5)"
         << '\n';
    return ostr;
@@ -81,10 +93,30 @@ Test_output test(F f, const std::vector<Dataset<N>>& dataset)
    to.false_positive_rate = static_cast<double>(to.false_positive) / to.negative;
    to.false_negative_rate = static_cast<double>(to.false_negative) / to.positive;
 
-   to.positive_predicive_value = static_cast<double>(to.true_positive) / (to.true_positive + to.false_positive);
-   to.negative_predicive_value = static_cast<double>(to.true_negative) / (to.true_negative + to.false_negative);
-   to.false_discovery_rate = static_cast<double>(to.false_positive) / (to.false_positive + to.true_positive);
-   to.false_omission_rate  = static_cast<double>(to.false_negative) / (to.false_negative + to.true_negative);
+   to.positive_predicive_value = static_cast<double>(to.true_positive) /
+      (to.true_positive + to.false_positive);
+   to.negative_predicive_value = static_cast<double>(to.true_negative) /
+      (to.true_negative + to.false_negative);
+   to.false_discovery_rate = static_cast<double>(to.false_positive) /
+      (to.false_positive + to.true_positive);
+   to.false_omission_rate  = static_cast<double>(to.false_negative) /
+      (to.false_negative + to.true_negative);
+
+   to.accuracy = static_cast<double>(to.true_positive + to.true_negative) /
+      to.sample_size;
+   to.f1_score = 2.0 * to.positive_predicive_value * to.true_positive_rate /
+      (to.positive_predicive_value + to.true_positive_rate);
+   to.matthews_correlation_coefficient =
+      (to.true_positive * to.true_negative -
+       to.false_positive * to.false_negative) /
+      std::sqrt(
+         (to.true_positive + to.false_positive) *
+         (to.true_positive + to.false_negative) *
+         (to.true_negative + to.false_positive) *
+         (to.true_negative + to.false_negative)
+         );
+   to.bookmaker_informednes = to.true_positive_rate + to.true_negative_rate - 1.0;
+   to.markednes = to.positive_predicive_value + to.negative_predicive_value - 1.0;
 
    to.mean_diff /= to.sample_size;
 

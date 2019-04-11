0114df5dac66b79e061d55f63106c6819371b6f0
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2019-04-11 22:33:11 +0200

    collecting more data about the evaluation of the testing sample

--- src/test.hpp
+++ src/test.hpp
@@ -8,6 +8,14 @@
 namespace perceptron {
 
 struct Test_output {
+   std::size_t sample_size{0};
+   std::size_t positive{0};
+   std::size_t negative{0};
+   std::size_t true_positive{0}; // true positive
+   std::size_t true_negative{0}; // true negative
+   std::size_t false_positive{0}; // false positive
+   std::size_t false_negative{0}; // false negative
+
    double mean_diff{0.0};
 
    friend std::ostream& operator<<(std::ostream&, const Test_output&);
@@ -17,6 +25,13 @@ std::ostream& operator<<(std::ostream& ostr, const Test_output& t)
 {
    ostr << "Test results\n"
            "============\n"
+        << "sample size = " << t.sample_size << '\n'
+        << "positive = " << t.positive << '\n'
+        << "negative = " << t.negative << '\n'
+        << "true_positive = " << t.true_positive << '\n'
+        << "true_negative = " << t.true_negative << '\n'
+        << "false_positive = " << t.false_positive << '\n'
+        << "false_negative = " << t.false_negative << '\n'
         << "mean abs diff = " << t.mean_diff << " (should be < 0.5)"
         << '\n';
    return ostr;
@@ -25,19 +40,25 @@ std::ostream& operator<<(std::ostream& ostr, const Test_output& t)
 template <class F, int N>
 Test_output test(F f, const std::vector<Dataset<N>>& dataset)
 {
-   double mean_diff = 0.;
+   Test_output to;
+   to.sample_size = dataset.size();
 
    for (const auto& d: dataset) {
-      const auto result = f.call(d.x);
-      mean_diff += std::abs(d.y - result);
+      const auto y = f.call(d.x);
+
+      if (d.y) to.positive++;
+      if (!d.y) to.negative++;
+      if (d.y && y) to.true_positive++;
+      if (!d.y && !y) to.true_negative++;
+      if (d.y && !y) to.false_positive++;
+      if (!d.y && y) to.false_negative++;
+
+      to.mean_diff += std::abs(d.y - y);
    }
 
-   mean_diff /= dataset.size();
+   to.mean_diff /= to.sample_size;
 
-   Test_output test_output;
-   test_output.mean_diff = mean_diff;
-
-   return test_output;
+   return to;
 }
 
 }
